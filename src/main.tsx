0f9304c2863031389038fb83aed467678d555232
Himeshika <himeshikanethmini@gmail.com>
2026-06-21 21:15:09 +0530

    fix index css issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+// @ts-expect-error: TypeScript lacks global type definitions for standard CSS imports
 import "./index.css";
 import App from "./App.tsx";
 
